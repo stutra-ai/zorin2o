0e8af75b771fe34abfb13aadfbf3c0d5fc67477f
stutra-ai <stutraai@gmail.com>
2026-09-11 05:02:03 -0400

    pc2

--- XNXX/src/main/kotlin/com/dune/XNXXPlugin.kt
+++ XNXX/src/main/kotlin/com/dune/XNXXPlugin.kt
@@ -2,6 +2,7 @@ package com.dune
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
 
 @CloudstreamPlugin
 class XNXXPlugin: Plugin() {
